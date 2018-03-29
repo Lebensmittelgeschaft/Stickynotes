0bfafc23cea539f71f7d38c94539e6745b66534b
yuvala15 <assa.yuval107@gmail.com>
2018-03-29 14:01:02 +0300

    Adding comments and moving irrelevant ones

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,6 +4,9 @@ import * as bodyParser from 'body-parser';
 import * as mongoose from 'mongoose';
 import { router } from './router';
 import { noteRouter } from './note/note.route';
+// import { notificationRouter } from './notification/notification.route';
+// import { roomRouter } from './room/room.route';
+// import { userRouter } from './user/user.route';
 import { config } from './config';
 
 
@@ -11,7 +14,7 @@ const app = express();
 
 (<any>mongoose).Promise = global.Promise;
 
-mongoose.connect(config.MONGOURI, { useMongoClient: true }, (err) => {
+mongoose.connect(config.MONGOURI, (err) => {
     if (err) {
         console.log("Error connecting to MongoDB")
         console.error(err);
@@ -32,8 +35,7 @@ app.use('/note', noteRouter);
 //app.use('/user', userRouter);
 
 app.listen(app.get('port'), () => {
-    console.log(`Stickynotes Server is running at http://localhost: ${app.get('port')} 
-                 in ${app.get('env')} mode`);
+    console.log(`Stickynotes Server is running at http://localhost:${app.get('port')} in ${app.get('env')} mode`);
     console.log('Press CTRL-C to stop\n');
 });
 
--- server/src/note/note.route.ts
+++ server/src/note/note.route.ts
@@ -6,25 +6,34 @@ export const noteRouter = Router();
 
 // GET methods
 noteRouter.get('/', async (req, res, next) => {
-  if (req.query.roomId){
-        try {    
-            const notes = await NoteController.getAllNotesInRoom(req.query.roomId);
-            if (notes) res.json(notes);  
-            else res.sendStatus(404);   
-        } catch (err) {
-            next(err);
-        }
+  if (req.query.roomId) {
+    try {
+      const notes = await NoteController.getAllNotesInRoom(req.query.roomId);
+      if (notes) res.json(notes);
+      else res.sendStatus(404);
+    } catch (err) {
+      next(err);
     }
+  }
 
-    res.sendStatus(400);
+  res.sendStatus(400);
+});
+
+noteRouter.get('/:id', async (req, res, next) => {
+  if (req.params.id) {
+    try {
+      const note = await NoteController.getNoteById(req.params.id);
+      if (note) res.json(note);
+      else res.sendStatus(404);
+    } catch (err) {
+      next(err);
+    }
+  }
 });
 
 // POST methods
 /**
  * Save new note
- * Most have parameters:
- *  sender - the sender of the message
- *  receiver - the receiver of the message
  */
 noteRouter.post('/', async (req, res, next) => {
   try {
@@ -39,32 +48,32 @@ noteRouter.post('/', async (req, res, next) => {
 
 // PUT methods
 /**
- * Update existing note
+ * Update an existing note
  */
 noteRouter.put('/', async (req, res, next) => {
   if (req.body._id) {
     try {
       const note = req.body;
-      const updatedNote = await NoteController.updateNoteById(note._id,note as Partial<INote>);
+      const updatedNote = await NoteController.updateNoteById(note._id, note as Partial<INote>);
       if (updatedNote) res.json(updatedNote);
-      else res.sendStatus(400);      
+      else res.sendStatus(400);
     } catch (err) {
       next(err);
     }
-  } 
+  }
   else res.sendStatus(400);
- 
+
 });
 
 // Delete methods
 /**
  * Delete note by id
  */
-noteRouter.delete('/', async (req, res, next) => {
-  if (req.body._id) {
+noteRouter.delete('/:id', async (req, res, next) => {
+  if (req.params.id) {
     try {
-      const deletedNote = await NoteController.deleteNoteById(req.body._id);
-      if (deletedNote) res.json(deletedNote);      
+      const deletedNote = await NoteController.deleteNoteById(req.params.id);
+      if (deletedNote) res.json(deletedNote);
       else res.sendStatus(404);
     } catch (err) {
       next(err);
